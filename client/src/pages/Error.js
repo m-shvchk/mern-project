f1b51729bb587728604edcc1ebc938d53e61aa11
m-shvchk <thirdinthequeue@gmail.com>
2022-06-17 21:51:53 +0300

    Error page

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,8 +1,44 @@
+import { Link } from 'react-router-dom'
+import img from '../images/not-found.svg'
+import styled from "styled-components";
+
 
 const Error = () => {
   return (
-    <h1>Error</h1>
+    <Wrapper className='full-page'>
+      <div>
+        <img src={img} alt='not found' />
+        <h3>Not Found</h3>
+        <p>The page you are looking for does not exist.</p>
+        <Link to='/'>back home</Link>
+      </div>
+    </Wrapper>
   )
 }
 
 export default Error
+
+const Wrapper = styled.main`
+  text-align: center;
+  img {
+    max-width: 600px;
+    display: block;
+    margin-bottom: 2rem;
+  }
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  h3 {
+    margin-bottom: 0.5rem;
+  }
+  p {
+    margin-top: 0;
+    margin-bottom: 0.5rem;
+    color: var(--grey-500);
+  }
+  a {
+    color: var(--primary-500);
+    text-decoration: underline;
+    text-transform: capitalize;
+  }
+`
