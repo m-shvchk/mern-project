7c129bc70a39f32c40980d6085f991c146fdf7ff
m-shvchk <thirdinthequeue@gmail.com>
2022-06-23 19:52:45 +0300

    not-found and error middleware

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 import express from 'express'
+import notFoundMiddleware from './middleware/not-found.js'
+import errorHandlerMiddleware from './middleware/error-handler.js'
+// !!! to use ES6 module import/export we should add "type": "module" in package.json
+// and then use full name of the imported file (with .js extension)
 const app = express()
+
+
+// middleware
 
 app.get('/', (req, res) => {
   res.send('Welcome!')
 })
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 5000
 
