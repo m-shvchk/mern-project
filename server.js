c6fbed5812062e821c74d7d22f709ce37a829cf8
m-shvchk <thirdinthequeue@gmail.com>
2022-06-30 20:11:36 +0300

    connecting front-end with server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 const app = express();
+// import cors from 'cors' // package to deal with CORS errors
 import dotenv from "dotenv";
 dotenv.config();
 import 'express-async-errors' // helps pass error to error handler middleware without try/catch + next() in the controller
@@ -19,11 +20,17 @@ import jobsRouter from './routes/jobsRoutes.js'
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 
+// app.use(cors()) // applying CORS package as a middleware
 app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.send("Welcome!");
+app.get("/", (req, res) => { // dummy route
+  res.json({msg: "Welcome!"});
 });
+
+app.get("/api/v1", (req, res) => {
+  res.json({msg: "API"});
+});
+
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', jobsRouter)
