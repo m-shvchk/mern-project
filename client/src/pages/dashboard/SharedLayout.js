da8a943713a45ee8f27a14c83cd9cecae983dad3
m-shvchk <thirdinthequeue@gmail.com>
2022-07-13 18:16:57 +0300

    shared layout

--- client/src/pages/dashboard/SharedLayout.js
+++ client/src/pages/dashboard/SharedLayout.js
@@ -1,16 +1,46 @@
-import { Outlet, Link } from 'react-router-dom' // Outlet component allows nested UI to show up when child routes are rendered 
-import Wrapper from '../../assets/wrappers/SharedLayout'
+import styled from "styled-components";
+import { useAppContext } from "../../context/appContext";
+import { Outlet } from "react-router-dom"; // Outlet component allows nested UI to show up when child routes are rendered
+import { Navbar, SmallSidebar, BigSidebar } from "../../components";
 
 const SharedLayout = () => {
+  const { user } = useAppContext();
   return (
-    <Wrapper>
-      <nav>
-        <Link to='all-jobs'>all jobs</Link>
-        <Link to='add-job'>add job</Link>
-      </nav>
-      <Outlet /> 
-    </Wrapper>
-  )
-}
+    <>
+      <Wrapper>
+        <main className="dashboard">
+          <SmallSidebar />
+          <BigSidebar />
+          <div>
+            <Navbar />
+            <div className="dashboard-page">
+              <Outlet />
+            </div>
+          </div>
+        </main>
+      </Wrapper>
+    </>
+  );
+};
 
-export default SharedLayout
+const Wrapper = styled.section`
+  .dashboard {
+    display: grid;
+    grid-template-columns: 1fr;
+  }
+  .dashboard-page {
+    width: 90vw;
+    margin: 0 auto;
+    padding: 2rem 0;
+  }
+  @media (min-width: 992px) {
+    .dashboard {
+      grid-template-columns: auto 1fr;
+    }
+    .dashboard-page {
+      width: 90%;
+    }
+  }
+`;
+
+export default SharedLayout;
