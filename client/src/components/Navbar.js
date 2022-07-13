9826194c12cc8219286f198d0a2c3cbbbee107be
m-shvchk <thirdinthequeue@gmail.com>
2022-07-13 23:27:46 +0300

    navbar functionality for sidebar toggle and logout user

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,48 @@
 import styled from "styled-components";
-import {FaHome} from "react-icons/fa"
+import { useState } from "react";
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
+import { useAppContext } from "../context/appContext";
+import Logo from "./Logo";
 
 const Navbar = () => {
-  return (
+    const { user, toggleSidebar, logoutUser } = useAppContext()
+    const [showLogout, setShowLogout] = useState(false)
+    return (
     <Wrapper>
-      <h4>Navbar</h4>
-      <FaHome />
-
+      <div className="nav-center">
+        <button
+          type="button"
+          className="toggle-btn"
+          onClick={toggleSidebar}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className="logo-text">dashboard</h3>
+        </div>
+        
+        <div className="btn-container">
+          <button
+            type="button"
+            className="btn"
+            onClick={() => setShowLogout(!showLogout)}
+          >
+            <FaUserCircle />
+                {user && user.name}
+            <FaCaretDown />
+          </button>
+          <div className={showLogout ? 'dropdown show-dropdown' : 'dropdown'}>
+            <button
+              type="button"
+              onClick={logoutUser}
+              className="dropdown-btn"
+            >
+              logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
