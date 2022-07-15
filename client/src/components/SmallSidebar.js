56087899c1411332a3608ceef153badf0f16c9c3
m-shvchk <thirdinthequeue@gmail.com>
2022-07-15 15:51:35 +0300

    small sidebar structure

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,9 +1,28 @@
 import styled from "styled-components";
+import { FaTimes } from "react-icons/fa";
+import { useAppContext } from "../context/appContext";
+import links from "../utils/links";
+import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
 
-const SmallSidebar = () => {
+export const SmallSidebar = () => {
   return (
     <Wrapper>
-      <h4>SmallSidebar</h4>
+      <div className="sidebar-container show-sidebar">
+        <div className="content">
+          <button
+            type="button"
+            className="close-btn"
+            onClick={() => console.log("toggle")}
+          >
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">nav links</div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
