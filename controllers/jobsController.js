4d87861c9d23a1650d517bbf9c70b0ae7fca3d4a
m-shvchk <thirdinthequeue@gmail.com>
2022-07-31 21:13:44 +0300

    getAllJobs controller

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -19,9 +19,14 @@ const createJob = async (req, res) => {
 const deleteJob = async (req, res) => {
   res.send("deleteJob");
 };
+
 const getAllJobs = async (req, res) => {
-  res.send("getAllJobs");
-};
+  const jobs = await Job.find({ createdBy: req.user.userId })
+  res
+    .status(StatusCodes.OK)
+    .json({ jobs, totalJobs: jobs.length, numOfPages: 1 })
+}
+
 const updateJob = async (req, res) => {
   res.send("updateJob");
 };
