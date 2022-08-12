8f2c13f49f40a304ebbb7078e36ec6f8f31d1b92
m-shvchk <thirdinthequeue@gmail.com>
2022-08-12 22:38:59 +0300

    fetching stats logic

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -24,6 +24,8 @@ import {
   EDIT_JOB_BEGIN,
   EDIT_JOB_SUCCESS,
   EDIT_JOB_ERROR,
+  SHOW_STATS_BEGIN,
+  SHOW_STATS_SUCCESS,
 } from "./actions";
 
 const token = localStorage.getItem("token");
@@ -52,6 +54,8 @@ export const initialState = {
   totalJobs: 0,
   numOfPages: 1,
   page: 1,
+  stats: {},
+  monthlyApplications: [],
 };
 const AppContext = React.createContext();
 
@@ -279,6 +283,25 @@ const AppProvider = ({ children }) => {
       // logoutUser() // !!! comented out while developping !!!
     }
   }
+
+  const showStats = async () => {
+    dispatch({ type: SHOW_STATS_BEGIN })
+    try {
+      const { data } = await authFetch('/jobs/stats')
+      dispatch({
+        type: SHOW_STATS_SUCCESS,
+        payload: {
+          stats: data.defaultStats,
+          monthlyApplications: data.monthlyApplications,
+        },
+      })
+    } catch (error) {
+console.log(error.response)
+      // logoutUser()
+    }
+
+clearAlert()
+  }
   
   return (
     <AppContext.Provider
@@ -296,6 +319,7 @@ const AppProvider = ({ children }) => {
         setEditJob,
         deleteJob,
         editJob,
+        showStats,
       }}
     >
       {children}
