f0441180d007693fc410a117ff739300c4f8f578
m-shvchk <thirdinthequeue@gmail.com>
2022-08-14 13:48:55 +0300

    stats page

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -7,6 +7,10 @@ import Navbar from "./Navbar";
 import FormRowSelect from "./FormRowSelect";
 import SearchContainer from "./SearchContainer";
 import JobsContainer from "./JobsContainer";
+import ChartsContainer from "./ChartsContainer";
+import StatsContainer from "./StatsContainer";
+import Loading from "./Loading";
+
 
 export {
   Logo,
@@ -18,4 +22,7 @@ export {
   FormRowSelect,
   SearchContainer,
   JobsContainer,
+  ChartsContainer,
+  StatsContainer,
+  Loading,
 };
