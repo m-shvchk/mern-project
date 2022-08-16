b144a61175dd5c78c48ba88c1f457c71f8a289e9
m-shvchk <thirdinthequeue@gmail.com>
2022-08-16 17:56:54 +0300

    chart container

--- client/src/components/ChartsContainer.js
+++ client/src/components/ChartsContainer.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const ChartsContainer = () => {
+import BarChart from './BarChart'
+import AreaChart from './AreaChart'
+import { useAppContext } from '../context/appContext'
+import styled from 'styled-components'
+
+export default function ChartsContainer() {
+  const [barChart, setBarChart] = useState(true)
+  const { monthlyApplications: data } = useAppContext()
+
   return (
-    <div>ChartsContainer</div>
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+
+      <button type='button' onClick={() => setBarChart(!barChart)}>
+        {barChart ? 'AreaChart' : 'BarChart'}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
   )
 }
 
-export default ChartsContainer
+const Wrapper = styled.section`
+  margin-top: 4rem;
+  text-align: center;
+  button {
+    background: transparent;
+    border-color: transparent;
+    text-transform: capitalize;
+    color: var(--primary-500);
+    font-size: 1.25rem;
+    cursor: pointer;
+  }
+  h4 {
+    text-align: center;
+    margin-bottom: 0.75rem;
+  }
+`
